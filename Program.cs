14b8849e4319b7a3a3834cc2b99549bf92273ec0
Diego Barbaresco <diego.bar@live.it>
2024-02-06 12:15:39 +0100

    app completed

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 using System;
+using Models;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(args[0]);
+        var stock = new Stock("EUR", 100m);
+        var displayChange = new DisplayChange(stock);
+
+        while(true)
+        {
+            Console.WriteLine("Insert a number");
+            string userInput = Console.ReadLine();
+            decimal decimalInput;
+
+            if(decimal.TryParse(userInput, out decimalInput))
+                stock.Price = decimalInput;
+            else
+                Console.WriteLine("Unable to parse '{0}'", userInput);
+        }
     }
 }
